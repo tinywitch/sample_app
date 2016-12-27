adba23966531111f2cee5d995569b9b2fcfab4e5
Thuy Ninh <thuyninh.esc@gmail.com>
2016-12-27 21:09:52 +0700

    add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root				'static_pages#home'
+
+  get 'help'	=>	'static_pages#help'
+  get 'about'	=>	'static_pages#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
